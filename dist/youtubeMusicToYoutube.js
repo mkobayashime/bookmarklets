d492abb6c6dda5e4f79317b2c4d560b31d6352eb
Masaki Kobayashi <contact@mkobayashi.me>
2022-05-25 17:36:39 +0900

    Add `youtubeMusicToYoutube.js`

---
+++ dist/youtubeMusicToYoutube.js
@@ -0,0 +1,1 @@
+javascript:(()=>{window.location.href%3Dwindow.location.href.replace(%22%2F%2Fmusic.youtube.com%2F%22%2C%22%2F%2Fyoutube.com%2F%22)%3B})()
---
+++ src/youtubeMusicToYoutube.js
@@ -0,0 +1,9 @@
+/**
+ * @title YouTube Music to YouTube
+ * @description Reopen currently playing music as video in normal YouTube
+ */
+
+window.location.href = window.location.href.replace(
+  "//music.youtube.com/",
+  "//youtube.com/"
+)
