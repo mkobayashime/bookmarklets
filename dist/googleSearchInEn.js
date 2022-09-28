0818485507b93cf6c75dea8e356eaec45815c72d
Masaki Kobayashi <contact@mkobayashi.me>
2022-09-28 20:25:55 +0900

    Build

--- dist/googleSearchInEn.js
+++ dist/googleSearchInEn.js
@@ -1,1 +1,1 @@
-javascript:(()=>{if(window.location.href.startsWith(%22https%3A%2F%2Fwww.google.com%2Fsearch%22))%7Bconst%20o%3Dnew%20URL(window.location.href)%3Bo.searchParams.set(%22lr%22%2C%22lang_en%22)%2Cwindow.location.href%3Do.toString()%7D})()
+javascript:(()=>{if(window.location.href.startsWith(%22https%3A%2F%2Fwww.google.com%2Fsearch%22))%7Blet%20o%3Dnew%20URL(window.location.href)%3Bo.searchParams.set(%22lr%22%2C%22lang_en%22)%2Cwindow.location.href%3Do.toString()%7D})()
